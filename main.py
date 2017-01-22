d12661a662259130e5064af2c3043520f6edd42d
TamerB <tamer.bhgt@gmail.com>
2017-01-22 07:34:50 +0200

    without timeout, queue

---
+++ main.py
@@ -0,0 +1,112 @@
+from flask import Flask, request
+from flask_socketio import SocketIO, send
+import time, threading, subprocess
+
+resources = {}
+clients = {}
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
+
+'''def check_resource(re, msg, request1):
+	request = request1
+	while True:
+		for r in resources:
+			if re != r['name'] and re.startswith(r['name'] + "/"):
+				handleMessage(msg)
+				return'''
+
+@socketio.on('message')
+def handleMessage(msg):
+	print(msg)
+	message = dict(msg)
+	print(message)
+
+	if message['resource'].endswith(" ") or message['resource'].endswith("/"):
+		message['resource'] = message['resource'].replace(message['resource'][len(message['resource']) - 1], "")
+
+	if 'type' in message:
+		if message['type'] == 'demand':
+			for r in resources:
+				if message['resource'] == r or message['resource'].startswith(r + "/"):
+					for c in clients[request.sid]:
+						if message['resource'] == c or message['resource'].startswith(c + "/"):
+							send(message['resource'] + " is already under your control")
+							return
+					send(message['resource'] + " is currently locked")
+					#threading.Thread(target=check_resource(r['name'], message, request)).start()
+					return
+
+				if r.startswith(message['resource']) and r in clients[request.sid]:
+					clients[request.sid][clients[request.sid].index(r)] = message['resource']
+					del r
+					resources[message['resource']] = time.time()
+					send(message['resource'] + " is now under your control")
+					return
+
+			resources[message['resource']] = time.time()
+			clients[request.sid].append(message['resource'])
+			send(message['resource'] + " is now under your control")
+			#check_timeout(message['resource'])
+			#threading.Thread(target=check_timeout(resources[resources.index(re)])).start()
+
+		elif message['type'] == 'release':
+			for c in clients[request.sid]:
+				if c == message['resource']:
+					for r in resources:
+						if c == r:
+							del r
+							clients[request.sid].remove(message['resource'])
+							send(message['resource'] + " released")
+							return
+				if message['resource'].startswith(c):
+					send("All of " + c + " is already under your control... You can release it as a whole if you wish")
+					return
+			send(message['resource'] + " is not under your control")
+
+
+		# A simple example of linux command on the last acquired resource for this user
+		elif message['type'] == 'command':
+			stdoutdata = subprocess.getoutput(message['resource'] + " " + clients[request.sid][len(clients[request.sid]) - 1])
+			send(stdoutdata)
+
+@socketio.on('connect')
+def test_connect():
+	print("%s connected" % (request.sid))
+	clients[request.sid] = []
+	print(request.sid + 'has entered the room.')
+	send("Welcome... You are now connected")
+
+@socketio.on('disconnect')
+def test_disconnect():
+	for r in resources:
+		if r == clients[request.sid]:
+			del r
+			del clients[request.sid]
+	print ("%s disconeected" % (request.sid))
+
+'''def check_timeout(r):
+	print("Yes, I'm here")
+	while True:
+		time.sleep(10)
+		print("Still here")
+		#for r in resources:
+		print("here")
+		print(time.time() - r['last_time'])
+		if time.time() - r['last_time'] > 10:
+			print("Here we go")
+			for s in clients:
+				for c in s:
+					if c == r['name']:
+						#resources.remove(r)
+						send(r + " released")
+						del r
+						s.remove(c)
+						break'''
+
+if __name__ == '__main__':
+	#threading.Thread(target=lambda: socketio.run(app)).start()
+	socketio.run(app)
+	#check_timeout()
+	#threading.Thread(target=check_timeOut()).start()
